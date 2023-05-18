b34b40bb6b95da30c9c323aaf6b6cbd9fa356285
pkuliyi2015 <liyi20129@gmail.com>
2023-05-18 07:24:24 +0000

    Partly fix the VAE reset performance issue

--- scripts/vae_optimize.py
+++ scripts/vae_optimize.py
@@ -652,6 +652,9 @@ class VAEHook:
 
 class Script(scripts.Script):
 
+    def __init__(self):
+        self.hooked = False
+
     def title(self):
         return "Tiled VAE"
 
@@ -695,18 +698,16 @@ class Script(scripts.Script):
         encoder = vae.encoder
         decoder = vae.decoder
 
-        # save original forward (only once)
-        if not hasattr(encoder, 'original_forward'): setattr(encoder, 'original_forward', encoder.forward)
-        if not hasattr(decoder, 'original_forward'): setattr(decoder, 'original_forward', decoder.forward)
-
         # undo hijack if disabled (in cases last time crashed)
         if not enabled:
-            if isinstance(encoder.forward, VAEHook):
-                encoder.forward.net = None
-                encoder.forward = encoder.original_forward
-            if isinstance(decoder.forward, VAEHook):
-                decoder.forward.net = None
-                decoder.forward = decoder.original_forward
+            if self.hooked:
+                if isinstance(encoder.forward, VAEHook):
+                    encoder.forward.net = None
+                    encoder.forward = encoder.original_forward
+                if isinstance(decoder.forward, VAEHook):
+                    decoder.forward.net = None
+                    decoder.forward = decoder.original_forward
+                self.hooked = False
             return
 
         if devices.get_optimal_device_name().startswith('cuda') and vae.device == devices.cpu and not vae_to_gpu:
@@ -719,6 +720,13 @@ class Script(scripts.Script):
             'color_fix':    color_fix, 
             'to_gpu':       vae_to_gpu,
         }
+
+        # save original forward (only once)
+        if not hasattr(encoder, 'original_forward'): setattr(encoder, 'original_forward', encoder.forward)
+        if not hasattr(decoder, 'original_forward'): setattr(decoder, 'original_forward', decoder.forward)
+
+        self.hooked = True
+        
         encoder.forward = VAEHook(encoder, encoder_tile_size, is_decoder=False, **kwargs)
         decoder.forward = VAEHook(decoder, decoder_tile_size, is_decoder=True,  **kwargs)
 
